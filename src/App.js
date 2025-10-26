207c30c54b23074ab81a35c27fe3a9d45c26f56c
mattilja88 <jaakko_m@hotmail.com>
2025-10-27 00:39:10 +0200

    React sivusto aluille

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
+    <div style={{ textAlign: "center" }}>
+      <h1>MathMaster</h1>
+      <iframe
+        title="MathMaster"
+        src="/unity/index.html"
+        style={{
+          width: "960px",
+          height: "540px",
+          border: "none",
+          background: "#000",
+        }}
+        allow="fullscreen; autoplay; gamepad"
+      />
     </div>
   );
 }
